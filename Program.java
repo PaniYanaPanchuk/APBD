8bf76940d2cfb0a24e498d52111d49badefc3284
s25401 <s25401@pjwstk.edu.pl>
2024-03-18 10:20:22 +0100

    Modyfikacja 3

--- Program.java
+++ Program.java
@@ -10,5 +10,14 @@ public class Program
         }
 
         int age = 19;
+
+        if (age > 18)
+        {
+            System.out.println("You are adult");
+        }
+        else
+        {
+            System.out.println("You are child");
+        }
     }
 }
