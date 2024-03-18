fd9670d8254e65282566d998833a3dbd20693336
s25401 <s25401@pjwstk.edu.pl>
2024-03-18 10:23:20 +0100

    Use static method averageValue()

--- Program.java
+++ Program.java
@@ -19,6 +19,10 @@ public class Program
         {
             System.out.println("You are child");
         }
+
+        int[] tab = {1,2,3,4,5,6,7,8,9,10};
+
+        System.out.println(avaregeValue(tab));
     }
 
     public static int avaregeValue(int[] tab)
