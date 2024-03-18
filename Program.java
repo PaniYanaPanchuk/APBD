06becc5655b02d19114464180f931a3727467511
s25401 <s25401@pjwstk.edu.pl>
2024-03-18 10:19:16 +0100

    Modyfikacja 1

--- Program.java
+++ Program.java
@@ -3,5 +3,10 @@ public class Program
     public static void main(String[] args)
     {
         System.out.println("Hello World!");
+
+        for (int i = 0; i < args.length; i++)
+        {
+            System.out.println(args[i]);
+        }
     }
 }
