c80ac9d39d018bf6e70ca14a19a55682deaa977e
s25401 <s25401@pjwstk.edu.pl>
2024-03-18 10:31:02 +0100

    Use static method maxValue()

--- Program.java
+++ Program.java
@@ -23,6 +23,8 @@ public class Program
         int[] tab = {1,2,3,4,5,6,7,8,9,10};
 
         System.out.println(avaregeValue(tab));
+
+        System.out.println(maxValue(tab));
     }
 
     public static int avaregeValue(int[] tab)
